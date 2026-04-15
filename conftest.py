121c2d4715a53c257e23068424e51edf10375d72
Your Name <you@example.com>
2026-04-15 17:49:39 +0300

    26.04.15 | fix tests/test_book.py and fix data.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 from main import BooksCollector
 import pytest
 
+
 @pytest.fixture(scope="function")
 def collector():
     return BooksCollector()
--- tests/test_book.py
+++ tests/test_book.py
@@ -5,26 +5,22 @@ import data
 class TestBooksCollector:
     def test_add_new_book_add_one_book_success(self, collector):
         collector.add_new_book(data.BOOK_5)
-        # input(f"\n{collector.books_genre}")
         assert collector.books_genre == {data.BOOK_5: ""}
 
     def test_add_new_book_add_two_book_success(self, collector):
         collector.add_new_book(data.BOOK_5)
         collector.add_new_book(data.BOOK_6)
-        # input(f"\n{collector.books_genre}")
         assert collector.books_genre == {
             data.BOOK_5: "", data.BOOK_6: ""}
 
     @pytest.mark.parametrize("name", ["A", "B" * 40])
     def test_add_new_book_valid_name_success(self, collector, name):
         collector.add_new_book(name)
-        # input(f"\n{collector.get_books_genre()}")
         assert name in collector.get_books_genre()
 
     @pytest.mark.parametrize("name", ["", "B" * 41])
     def test_add_new_book_invalid_name_length_41(self, collector, name):
         collector.add_new_book(name)
-        # input(f"\n{collector.get_books_genre()}")
         assert name not in collector.get_books_genre()
 
     def test_set_book_genre_existing_genre(self, collector):
@@ -37,7 +33,6 @@ class TestBooksCollector:
         collector.add_new_book(data.BOOK_4)
         collector.set_book_genre(data.BOOK_3, "Фантастика")
         collector.set_book_genre(data.BOOK_4, "Фантастика")
-        # input(f"\n{collector.get_books_with_specific_genre("Фантастика")}")
         assert len(collector.get_books_with_specific_genre("Фантастика")) == 2
 
     def test_get_books_for_children(self, collector):
@@ -45,7 +40,6 @@ class TestBooksCollector:
         collector.add_new_book(data.BOOK_2)
         collector.set_book_genre(data.BOOK_1, "Мультфильмы")
         collector.set_book_genre(data.BOOK_2, "Ужасы")
-        # input(f"\n {collector.get_books_for_children()}")
         assert collector.get_books_for_children() == [data.BOOK_1]
 
     def test_add_book_in_favorites_one_book_list_of_book(self, collector):
@@ -69,3 +63,10 @@ class TestBooksCollector:
 
     def test_get_book_genre_non_existent_book(self, collector):
         assert collector.get_book_genre('Несуществующая') is None
+
+    def test_get_books_genre(self, collector):
+        collector.add_new_book(data.BOOK_7)
+        collector.set_book_genre(data.BOOK_7, "Мультфильмы")
+        result = collector.get_books_genre()
+        assert isinstance(result, dict) and result == {
+            data.BOOK_7: "Мультфильмы"}
